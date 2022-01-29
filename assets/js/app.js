0f5b63e7f279d1172eb958d4540eb9268a4a3438
themanwiththeplan-eng <dakotadunn5@gmail.com>
2022-01-29 01:29:42 -0800

    pulled from github api to show my repositories

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,18 @@
 // var url  = giphy for card top
-// var gitUrl = Github for repo search 
+const gitUrl = `https://api.github.com/users/themanwiththeplan-eng/repos`
+
+$(document).ready(function(){
+    $.ajax({
+        method: 'GET',
+        url: gitUrl
+    }).then(function(response){
+        console.log(response);
+        for(let i = 0; i < response.length; i++){
+            const li = $("<li>");
+            const a = $("<a>").attr("href", response[i].html_url);
+            li.append(a);
+            a.text(response[i].html_url)
+            $(".repos").append(li);
+        }
+    })
+})
