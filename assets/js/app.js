65bcf31bdbcfce3cc850ddab7d5207e4ef039978
Dakota Dunn <dakotadunn5@gmail.com>
2022-02-01 23:25:30 -0800

    added another api call, made links another color, and made it dark so that there is no worry about eyesight

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 // var url  = giphy for card top
 const gitUrl = `https://api.github.com/users/themanwiththeplan-eng/repos`
-
+const giphyKey = `sh3X7U8cBYQJ1zgGMBfuN9SYZrXPt6uN`;
+const giphyUrl = `https://api.giphy.com/v1/stickers/search?api_key=${giphyKey}&q=hacker&limit=1&rating=r&SameSite=Strict`
 $(document).ready(function(){
     $.ajax({
         method: 'GET',
@@ -10,9 +11,19 @@ $(document).ready(function(){
         for(let i = 0; i < response.length; i++){
             const li = $("<li>");
             const a = $("<a>").attr("href", response[i].html_url);
+            a.attr("target", "_blank");
             li.append(a);
             a.text(response[i].html_url)
             $(".repos").append(li);
+
         }
+    })
+    $.ajax({
+        method: 'GET',
+        url: giphyUrl
+    }).then(function(response){
+        const currentGif = response.data[0].images.original.url;
+        $(".image").css("background-image", `url(${currentGif})`);
+        
     })
 })
